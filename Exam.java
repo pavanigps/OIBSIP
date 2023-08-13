79816a5d55955c2ac5412d3f56b9856bec83d441
pavanigps <137436986+pavanigps@users.noreply.github.com>
2023-08-13 22:46:10 +0530

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,15 @@
+public class Exam {
+    private Question[] questions;
+    private int timeLimit;
+
+    public Exam(Question[] questions, int timeLimit) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+    }
+
+    public int startExam() {
+        // Implement the exam logic here
+        // Return the user's score
+        return 0; // Replace with actual score calculation
+    }
+}
